c2e4fbb1d3f13d86ac5f74f70197404f35c713f6
Fbernabe1609 <fbernabe160903@gmail.com>
2022-11-16 20:20:24 +0100

    Clase

---
+++ src/main/java/org/example/Alumno.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Alumno {
+    String nombre;
+    float nota;
+
+    public Alumno(String nombre, float nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+}
---
+++ src/main/java/org/example/Medac.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Medac {
+    String Olmedo;
+
+    public Medac(String olmedo) {
+        Olmedo = olmedo;
+    }
+
+    public String getOlmedo() {
+        return Olmedo;
+    }
+
+    public void setOlmedo(String olmedo) {
+        Olmedo = olmedo;
+    }
+}
