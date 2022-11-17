ca0c10f7baf4b7f6b3a606e85e69d8cbc73dedc4
Fbernabe1609 <fbernabe160903@gmail.com>
2022-11-17 20:18:48 +0100

    Clase

--- src/main/java/ejemplo_dependencias_constructor/A.java
+++ src/main/java/ejemplo_dependencias_constructor/A.java
@@ -3,10 +3,9 @@ package ejemplo_dependencias_constructor;
 public class A {
     B b;
 
-    public void setB(B b) {
+    public A(B b) {
         this.b = b;
     }
-
     public float tenPercent() {
         return (float) (b.sum() * 0.1);
     }
--- src/main/java/ejemplo_dependencias_constructor/Main.java
+++ src/main/java/ejemplo_dependencias_constructor/Main.java
@@ -2,7 +2,7 @@ package ejemplo_dependencias_constructor;
 
 public class Main {
     public static void main(String[] args) {
-        A a = new A();
+        A a = new A(new B());
         System.out.println(a.tenPercent());;
     }
 }
